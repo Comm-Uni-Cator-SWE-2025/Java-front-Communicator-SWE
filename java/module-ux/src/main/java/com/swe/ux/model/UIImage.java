e2662a8f746981db2293dbfff9b9a67fccd000c4
alonot <priyanshup27092004@gmail.com>
2025-11-07 02:43:31 +0530

    Sending back response of UI_Update to Core

--- java/module-ux/src/main/java/com/swe/ux/model/UIImage.java
+++ java/module-ux/src/main/java/com/swe/ux/model/UIImage.java
@@ -4,8 +4,64 @@
 package com.swe.ux.model;
 
 import java.awt.image.BufferedImage;
+import java.util.Objects;
 
-public record UIImage(
-    BufferedImage image,
-    String ip
-) {}
+public final class UIImage {
+    private final BufferedImage image;
+    private final String ip;
+    private byte isSuccess;
+
+    public UIImage(
+        BufferedImage image,
+        String ip,
+        byte isSuccess
+    ) {
+        this.image = image;
+        this.ip = ip;
+        this.isSuccess = isSuccess;
+    }
+
+    public void setIsSuccess(boolean val) {
+        isSuccess = (byte) (val ? 1 : 0);
+    }
+
+    public BufferedImage image() {
+        return image;
+    }
+
+    public String ip() {
+        return ip;
+    }
+
+    public byte isSuccess() {
+        return isSuccess;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        var that = (UIImage) obj;
+        return Objects.equals(this.image, that.image) &&
+            Objects.equals(this.ip, that.ip) &&
+            this.isSuccess == that.isSuccess;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(image, ip, isSuccess);
+    }
+
+    @Override
+    public String toString() {
+        return "UIImage[" +
+            "image=" + image + ", " +
+            "ip=" + ip + ", " +
+            "isSuccess=" + isSuccess + ']';
+    }
+
+}
