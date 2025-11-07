ae268d511f8517a3745a56a0072cc3a98b21b590
Logan <112201016@smail.iitpkd.ac.in>
2025-11-07 11:00:34 +0530

    Checkstyle change in networking front

--- java/module-networking/src/main/java/com/swe/networking/NetworkFront.java
+++ java/module-networking/src/main/java/com/swe/networking/NetworkFront.java
@@ -4,6 +4,10 @@ import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
+/**
+ * The frontend networking class to connect to the RPC and to the core
+ * netwroking.
+ */
 public class NetworkFront implements AbstractController, AbstractNetworking {
 
     /** Variable to store the function mappings. */
@@ -13,19 +17,19 @@ public class NetworkFront implements AbstractController, AbstractNetworking {
     private int functionCount = 1;
 
     @Override
-    public void sendData(byte[] data, ClientNode[] dest, int module, int priority) {
-        int dataLength = data.length;
+    public void sendData(final byte[] data, final ClientNode[] dest, final int module, final int priority) {
+        final int dataLength = data.length;
         int destSize = 0;
         for (ClientNode record : dest) {
-            byte[] hostName = record.hostName().getBytes(StandardCharsets.UTF_8);
+            final byte[] hostName = record.hostName().getBytes(StandardCharsets.UTF_8);
             destSize += 1 + hostName.length + Integer.BYTES; // 1 byte length + host + port
         }
         // 1 - data length 1 - dest count 1 - module 1 - priority
         final int bufferSize = dataLength + destSize + 4 * Integer.BYTES;
-        ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+        final ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
         buffer.putInt(dest.length);
         for (ClientNode record : dest) {
-            byte[] hostName = record.hostName().getBytes(StandardCharsets.UTF_8);
+            final byte[] hostName = record.hostName().getBytes(StandardCharsets.UTF_8);
             buffer.put((byte) hostName.length);
             buffer.put(hostName);
             buffer.putInt(record.port());
@@ -34,51 +38,51 @@ public class NetworkFront implements AbstractController, AbstractNetworking {
         buffer.put(data);
         buffer.putInt(module);
         buffer.putInt(priority);
-        byte[] args = buffer.array();
+        final byte[] args = buffer.array();
 
-        // TODO: Call RPC broadcast
+        // TOD: Call RPC broadcast
     }
 
     @Override
-    public void broadcast(byte[] data, int module, int priority) {
-        int dataLength = data.length;
+    public void broadcast(final byte[] data, final int module, final int priority) {
+        final int dataLength = data.length;
         final int bufferSize = dataLength + 3 * Integer.BYTES;
-        ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+        final ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
         buffer.putInt(dataLength);
         buffer.put(data);
         buffer.putInt(module);
         buffer.putInt(priority);
-        byte[] args = buffer.array();
+        final byte[] args = buffer.array();
 
-        // TODO: Call RPC broadcast
+        // TOD: Call RPC broadcast
     }
 
     @Override
-    public void subscribe(int name, MessageListener function) {
+    public void subscribe(final int name, final MessageListener function) {
         listeners.put(name, function);
         final int bufferSize = Integer.BYTES;
-        ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+        final ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
         buffer.putInt(name);
-        byte[] args = buffer.array();
+        final byte[] args = buffer.array();
 
-        // TODO: Call RPC Add function
+        // TOD: Call RPC Add function
     }
 
     @Override
-    public void removeSubscription(int name) {
+    public void removeSubscription(final int name) {
         final int bufferSize = Integer.BYTES;
-        ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+        final ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
         buffer.putInt(name);
-        byte[] args = buffer.array();
+        final byte[] args = buffer.array();
 
-        // TODO: Call RPC remove
+        // TOD: Call RPC remove
     }
 
     @Override
-    public void addUser(ClientNode deviceAddress, ClientNode mainServerAddress) {
+    public void addUser(final ClientNode deviceAddress, final ClientNode mainServerAddress) {
         final int bufferSize = 2 + deviceAddress.hostName().length() + mainServerAddress.hostName().length()
                 + 2 * Integer.BYTES;
-        ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+        final ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
         byte[] hostName = deviceAddress.hostName().getBytes(StandardCharsets.UTF_8);
         buffer.put((byte) hostName.length);
         buffer.put(hostName);
@@ -87,9 +91,9 @@ public class NetworkFront implements AbstractController, AbstractNetworking {
         buffer.put((byte) hostName.length);
         buffer.put(hostName);
         buffer.putInt(mainServerAddress.port());
-        byte[] args = buffer.array();
+        final byte[] args = buffer.array();
 
-        // TODO: Call RPC addUser
+        // TOD: Call RPC addUser
     }
 
     /**
@@ -97,15 +101,14 @@ public class NetworkFront implements AbstractController, AbstractNetworking {
      *
      * @param data the data to send
      */
-    public void networkFrontCallSubscriber(byte[] data) {
-        int dataSize = data.length - 1;
-        ByteBuffer buffer = ByteBuffer.wrap(data);
-        int module = buffer.getInt();
-        byte[] newData = new byte[dataSize];
-        MessageListener function = listeners.get(module);
+    public void networkFrontCallSubscriber(final byte[] data) {
+        final int dataSize = data.length - 1;
+        final ByteBuffer buffer = ByteBuffer.wrap(data);
+        final int module = buffer.getInt();
+        final byte[] newData = new byte[dataSize];
+        final MessageListener function = listeners.get(module);
         if (function != null) {
             function.receiveData(newData);
         }
-
     }
 }
