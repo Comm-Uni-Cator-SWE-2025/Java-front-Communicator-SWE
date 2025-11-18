4dc2a278b60d825aecbf2b46511b5dfb8db82857
Sandeep Kumar <sandeepkumar75255@gmail.com>
2025-11-18 15:54:13 +0530

    feat: implement participant zoom, dynamic grid layout, and visibility tracking

--- java/module-ux/src/main/java/com/swe/ux/viewmodel/ScreenNVideoModel.java
+++ java/module-ux/src/main/java/com/swe/ux/viewmodel/ScreenNVideoModel.java
@@ -7,18 +7,24 @@ import com.swe.screenNVideo.AbstractRPC;
 import com.swe.screenNVideo.DummyRPC;
 import com.swe.screenNVideo.RImage;
 import com.swe.screenNVideo.Utils;
+import com.swe.ux.binding.BindableProperty;
 import com.swe.ux.model.UIImage;
 
 import java.awt.image.BufferedImage;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Consumer;
 
 public class ScreenNVideoModel extends BaseViewModel {
-    
+
     private Consumer<UIImage> onImageReceived;
 
     private AbstractRPC rpc;
 
     private static ScreenNVideoModel INSTANCE;
+
+    // Property to track visible participants (IPs)
+    public final BindableProperty<Set<String>> visibleParticipants = new BindableProperty<>(new HashSet<>(), "visibleParticipants");
 
     public static ScreenNVideoModel getInstance() {
         if (INSTANCE == null) {
@@ -26,17 +32,25 @@ public class ScreenNVideoModel extends BaseViewModel {
         }
         return INSTANCE;
     }
-    
+
     private ScreenNVideoModel() {
         this.rpc = DummyRPC.getInstance();
-        
+
         initComponents();
     }
 
     public void setOnImageReceived(Consumer<UIImage> onImageReceived) {
         this.onImageReceived = onImageReceived;
     }
-    
+
+    /**
+     * Updates the list of currently visible participants.
+     * Called by the View when layout or scroll changes.
+     */
+    public void updateVisibleParticipants(Set<String> visibleIps) {
+        visibleParticipants.set(visibleIps);
+    }
+
     private void initComponents() {
         rpc.subscribe(Utils.UPDATE_UI, (args) -> {
             final RImage rImage = RImage.deserialize(args);
